aa75d445095da2e337f23b7e88dd8156807a8936
ChrisScottThomas <chris.scott-thomas@burendo.com>
2022-09-15 16:10:59 +0100

    org imported
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- org.tf
+++ org.tf
@@ -6,7 +6,12 @@ resource "aws_organizations_organization" "burendo" {
   aws_service_access_principals = [
     "cloudtrail.amazonaws.com",
     "config.amazonaws.com",
+    "controltower.amazonaws.com",
     "sso.amazonaws.com"
+  ]
+
+  enabled_policy_types = [
+    "SERVICE_CONTROL_POLICY",
   ]
 
   feature_set = "ALL"
