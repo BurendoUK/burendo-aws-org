f95009a80e1cfe5b1068c7b2bc9fec272a432377
ChrisScottThomas <chris.scott-thomas@burendo.com>
2022-09-15 16:45:01 +0100

    rm comments
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- org.tf
+++ org.tf
@@ -1,8 +1,5 @@
 resource "aws_organizations_organization" "burendo" {
 
-  # List of AWS service principal names for which 
-  # you want to enable integration with your organization
-
   aws_service_access_principals = [
     "cloudtrail.amazonaws.com",
     "config.amazonaws.com",
