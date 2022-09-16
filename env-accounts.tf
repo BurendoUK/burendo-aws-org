a0801c37a28c3e63521d5cc50f6643a39a4116e9
ChrisScottThomas <chris.scott-thomas@burendo.com>
2022-09-16 10:39:24 +0100

    Do no directl display service account emails
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- env-accounts.tf
+++ env-accounts.tf
@@ -1,26 +1,26 @@
 resource "aws_organizations_account" "burendo_dev" {
   name      = "burendo-dev"
-  email     = "dev.engineering@burendo.com"
+  email     = lookup(local.email, "burendo-dev")
   tags      = merge(local.tags, { Name = "burendo-dev" })
   parent_id = aws_organizations_organizational_unit.infrastructure.id
 }
 
 resource "aws_organizations_account" "burendo_management_dev" {
   name      = "burendo-management-dev"
-  email     = "mgmt.dev.engineering@burendo.com"
+  email     = lookup(local.email, "burendo-mgmt-dev")
   tags      = merge(local.tags, { Name = "burendo-management-dev" })
   parent_id = aws_organizations_organizational_unit.infrastructure.id
 }
 
 resource "aws_organizations_account" "burendo_production" {
   name      = "burendo-production"
-  email     = "production@burendo.com"
+  email     = lookup(local.email, "burendo-prod")
   tags      = merge(local.tags, { Name = "burendo-production" })
   parent_id = aws_organizations_organizational_unit.infrastructure.id
 }
 
 resource "aws_organizations_account" "burendo" {
   name  = "Burendo"
-  email = "admin@burendo.com"
+  email = lookup(local.email, "burendo-mgmt")
   tags  = merge(local.tags, { Name = "Burendo" })
 }
--- sec-accounts.tf
+++ sec-accounts.tf
@@ -1,13 +1,13 @@
 resource "aws_organizations_account" "audit" {
   name      = "Audit"
-  email     = "audit@burendo.com"
+  email     = lookup(local.email, "audit")
   tags      = merge(local.tags, { Name = "Audit" })
   parent_id = aws_organizations_organizational_unit.security.id
 }
 
 resource "aws_organizations_account" "log_archive" {
   name      = "Log Archive"
-  email     = "log-archive@burendo.com"
+  email     = lookup(local.email, "log-archive")
   tags      = merge(local.tags, { Name = "Log Archive" })
   parent_id = aws_organizations_organizational_unit.security.id
 }
