b38ed2522abe0398c31fb55d3b125f34617cda07
Chris Scott-Thomas <chris.scott-thomas@burendo.com>
2022-09-16 15:17:29 +0100

    Ep76 add org scp (#3)
    * rm githooks
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * rm gitmodules
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * Make executable
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * Fix githooks
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * Import AWS managed SCP and its attachments
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

---
+++ scp.tf
@@ -0,0 +1,18 @@
+locals {
+  organization_policy_full_access_id = "p-FullAWSAccess"
+}
+
+resource "aws_organizations_policy_attachment" "infrastructure" {
+  policy_id = local.organization_policy_full_access_id
+  target_id = aws_organizations_organizational_unit.infrastructure.id
+}
+
+resource "aws_organizations_policy_attachment" "security" {
+  policy_id = local.organization_policy_full_access_id
+  target_id = aws_organizations_organizational_unit.security.id
+}
+
+resource "aws_organizations_policy_attachment" "management" {
+  policy_id = local.organization_policy_full_access_id
+  target_id = aws_organizations_account.burendo.id
+}
