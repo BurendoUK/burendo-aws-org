e9bebadaa3601ba4c5d26f55fb0b6f950cac1197
ChrisScottThomas <chris.scott-thomas@burendo.com>
2022-09-16 08:16:00 +0100

    Import OUs
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

---
+++ ou.tf
@@ -0,0 +1,9 @@
+resource "aws_organizations_organizational_unit" "infrastructure" {
+  name      = "Infrastructure"
+  parent_id = aws_organizations_organization.burendo.roots[0].id
+}
+
+resource "aws_organizations_organizational_unit" "security" {
+  name      = "Security"
+  parent_id = aws_organizations_organization.burendo.roots[0].id
+}
