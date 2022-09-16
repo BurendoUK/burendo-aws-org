fd3190e1244d49fbb0beba1c032fb13ffe9e6c7b
ChrisScottThomas <chris.scott-thomas@burendo.com>
2022-09-16 08:48:09 +0100

    Import sandbox accounts
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

---
+++ sandbox-accounts.tf
@@ -0,0 +1,20 @@
+resource "aws_organizations_account" "leec_sandbox" {
+  name      = "LeeCSandbox"
+  email     = "lee.clark78@burendo.com"
+  tags      = merge(local.tags, { Name = "LeeCSandbox" })
+  parent_id = aws_organizations_organizational_unit.infrastructure.id
+}
+
+resource "aws_organizations_account" "rowang_sandbox" {
+  name      = "RowanGSandbox"
+  email     = "rowan.gill@burendo.com"
+  tags      = merge(local.tags, { Name = "RowanGSandbox" })
+  parent_id = aws_organizations_organizational_unit.infrastructure.id
+}
+
+resource "aws_organizations_account" "zesh_sandbox" {
+  name      = "ZeshSandbox"
+  email     = "zeshan.zoheb@burendo.com"
+  tags      = merge(local.tags, { Name = "ZeshSandbox" })
+  parent_id = aws_organizations_organizational_unit.infrastructure.id
+}
