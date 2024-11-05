7c6f07a9a39ef1f023b0b205e13f5249b5f0aae4
JoshuaDouceBurendo <joshua.douce@burendo.com>
2024-11-05 16:23:05 +0000

    Replace FGFS client account

--- client-accounts.tf
+++ client-accounts.tf
@@ -2,9 +2,9 @@
 # the users of this account hence managing who can access the clients accounts via an assumed role. 
 # When the engagement concludes delete this aws account. 
 
-resource "aws_organizations_account" "frasers_group_financial_services" {
-  name      = "FrasersGroupFinancialServices"
-  email     = "joshua.douce+fgfs@burendo.com"
-  tags      = { Name = "FrasersGroupFinancialServices" }
+resource "aws_organizations_account" "fgfs" {
+  name      = "FGFS"
+  email     = local.client_aws_email
+  tags      = merge(local.tags, { Name = "FrasersGroupFinancialServices" })
   parent_id = aws_organizations_organizational_unit.clients.id
 }
