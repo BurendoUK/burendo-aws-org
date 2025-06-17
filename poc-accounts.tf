246b5c2429be9b1b2a7b53598432a60fbc4e6a66
Chris Ramsden <chris.ramsden@burendo.com>
2025-06-17 09:15:33 +0100

    Use existing client email for POC accounts

--- poc-accounts.tf
+++ poc-accounts.tf
@@ -4,14 +4,14 @@
 # When the engagement concludes delete these aws account. 
 resource "aws_organizations_account" "nhs_data_dashboard_tooling" {
   name      = "nhs-data-dashboard-tooling"
-  email     = local.poc_aws_email
+  email     = local.client_aws_email
   tags      = merge(local.tags, { Name = "NHSDataDashboardTooling" })
   parent_id = aws_organizations_organizational_unit.clients.id
 }
 
 resource "aws_organizations_account" "nhs_data_dashboard_application" {
   name      = "nhs-data-dashboard-application"
-  email     = local.poc_aws_email
+  email     = local.client_aws_email
   tags      = merge(local.tags, { Name = "NHSDataDashboardApplication" })
   parent_id = aws_organizations_organizational_unit.clients.id
 }
