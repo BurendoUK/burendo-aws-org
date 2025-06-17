cf4cba9ec11f4c27e83e0611ae7c02c95188b385
Chris Ramsden <tvking@users.noreply.github.com>
2025-06-17 09:26:46 +0100

    Create 2 POC accounts for NHS Data (#13)
    * Create 2 POC accounts for NHS Data
    
    * Use existing client email for POC accounts
    
    ---------
    
    Co-authored-by: Chris Ramsden <chris.ramsden@burendo.com>

---
+++ poc-accounts.tf
@@ -0,0 +1,17 @@
+# These accounts exists to allow Burendo to build POC solutions for clients or potential clients 
+
+# The following accounts allow the creation of an NHS data dashboard solution.
+# When the engagement concludes delete these aws account. 
+resource "aws_organizations_account" "nhs_data_dashboard_tooling" {
+  name      = "nhs-data-dashboard-tooling"
+  email     = local.client_aws_email
+  tags      = merge(local.tags, { Name = "NHSDataDashboardTooling" })
+  parent_id = aws_organizations_organizational_unit.clients.id
+}
+
+resource "aws_organizations_account" "nhs_data_dashboard_application" {
+  name      = "nhs-data-dashboard-application"
+  email     = local.client_aws_email
+  tags      = merge(local.tags, { Name = "NHSDataDashboardApplication" })
+  parent_id = aws_organizations_organizational_unit.clients.id
+}
